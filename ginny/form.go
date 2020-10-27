cf2c810575118574522aec5d83472c24f365c5ea
linyyyang <linyyyang@tencent.com>
2020-10-27 14:12:37 +0800

    rebuild

--- ginny/form.go
+++ ginny/form.go
@@ -2,6 +2,7 @@ package ginny
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,16 +17,25 @@ func Post(ctx *gin.Context, ptr interface{}) error {
 
 // shouldBindForm
 func shouldBindForm(ctx *gin.Context, ptr interface{}) error {
-	if err := ctx.Request.ParseForm(); err != nil {
-		return err
-	}
-	if err := ctx.Request.ParseMultipartForm(defaultMemory); err != nil {
-		if err != http.ErrNotMultipart {
+	h := ctx.GetHeader("content-type")
+	if h == "application/x-www-form-urlencoded" {
+		if err := ctx.Request.ParseForm(); err != nil {
 			return err
 		}
-	}
-	if err := mappingByPtr(ptr, ctx.Request.Form, "json"); err != nil {
-		return err
+		if err := mappingByPtr(ptr, ctx.Request.PostForm, "json"); err != nil {
+			return err
+		}
+	} else if strings.Contains(h, "multipart/form-data") {
+		if err := ctx.Request.ParseMultipartForm(defaultMemory); err != nil {
+			if err != http.ErrNotMultipart {
+				return err
+			}
+		}
+		if err := mappingByPtr(ptr, ctx.Request.Form, "json"); err != nil {
+			return err
+		}
+	} else {
+		return ctx.BindJSON(ptr)
 	}
 	return nil
 }
