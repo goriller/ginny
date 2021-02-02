6bc222daae76228f866e6c6e6fa2789318f826c0
linyyyang <linyyyang@tencent.com>
2021-02-02 17:28:15 +0800

    update

--- ginny/binding.go
+++ ginny/binding.go
@@ -1,20 +1,35 @@
 package ginny
 
 import (
+	"fmt"
+	"reflect"
+
 	"github.com/gin-gonic/gin"
 )
 
 // Query
 func Query(ctx *gin.Context, ptr interface{}) error {
+	kind := reflect.TypeOf(ptr)
+	if kind.Kind() != reflect.Ptr {
+		return fmt.Errorf("invalid type, must be pointer")
+	}
 	return ctx.ShouldBindQuery(ptr)
 }
 
 // PathVariable
 func PathVariable(ctx *gin.Context, ptr interface{}) error {
+	kind := reflect.TypeOf(ptr)
+	if kind.Kind() != reflect.Ptr {
+		return fmt.Errorf("invalid type, must be pointer")
+	}
 	return ctx.ShouldBindUri(ptr)
 }
 
 // Param
 func Param(ctx *gin.Context, ptr interface{}) error {
+	kind := reflect.TypeOf(ptr)
+	if kind.Kind() != reflect.Ptr {
+		return fmt.Errorf("invalid type, must be pointer")
+	}
 	return ctx.ShouldBind(ptr)
 }
