667b9f45b57db2ea8ab7f63447d8e8ea72d9e543
linyyyang <linyyyang@tencent.com>
2021-07-28 10:30:50 +0800

    ✨ feat():支持环境变量配置

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/google/wire"
 	"github.com/spf13/viper"
@@ -13,16 +15,32 @@ func New(path string) (*viper.Viper, error) {
 		err error
 		v   = viper.New()
 	)
+	log.Println("Loading config...")
 	v.AddConfigPath(".")
 	v.SetConfigFile(string(path))
 
 	if err := v.ReadInConfig(); err == nil {
-		fmt.Printf("use config file -> %s\n", v.ConfigFileUsed())
+		log.Printf("Config %s loaded successfully...", v.ConfigFileUsed())
 	} else {
 		return nil, err
 	}
+	log.Println("Getting environment variables...")
+	for _, k := range viper.AllKeys() {
+		value := viper.GetString(k)
+		if strings.HasPrefix(value, "${") && strings.HasSuffix(value, "}") {
+			viper.Set(k, getEnv(strings.TrimSuffix(strings.TrimPrefix(value, "${"), "}")))
+		}
+	}
 
 	return v, err
+}
+
+func getEnv(env string) string {
+	res := os.Getenv(env)
+	if len(env) == 0 {
+		return ""
+	}
+	return res
 }
 
 var ProviderSet = wire.NewSet(New)
