ee8d3621a1698eb514160b924cb89f67c0c0d06b
linyyyang <linyyyang@tencent.com>
2022-07-25 10:56:13 +0800

    fix: remove tags.extract

--- logging/interceptors.go
+++ logging/interceptors.go
@@ -55,11 +55,6 @@ func commonFields(ctx context.Context, kind, service, method string, typ interce
 		"action", "/" + service + "/" + method,
 		"protocol", "grpc/" + kind + "/" + string(typ),
 	}
-	//
-	preTags := tags.Extract(ctx)
-	for k, v := range preTags.Values() {
-		fields = append(fields, []string{k, v}...)
-	}
 
 	return fields
 }
